42c61ec88ecfe4519758b0b2f37f3cf63ed7850c
jumplight100 <ivan.fateev.nz@gmail.com>
2025-03-22 10:40:06 +1300

    working minting

--- Back End/hardhat.config.js
+++ Back End/hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    compilers: [
+      { version: "0.8.22" },
+      { version: "0.8.28" }
+    ],
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
+
+
---
+++ Back End/ignition/modules/deploy.ts
@@ -0,0 +1,8 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const MyModule = buildModule("MyModule", (m) => {
+  const myToken = m.contract("MyToken", ["0xf4a2452d358F8232236a5C3235873fbB34463303"]);
+  return { myToken };
+});
+
+export default MyModule;
