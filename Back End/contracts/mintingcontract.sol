6498640f412ad7fa107d3789ae172cb4c7e898ea
jumplight100 <ivan.fateev.nz@gmail.com>
2025-03-22 13:17:53 +1300

    added minting for unique ids

--- Back End/contracts/mintingcontract.sol
+++ Back End/contracts/mintingcontract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.22;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC721, Ownable {
+contract MyToken2 is ERC721, Ownable {
     uint256 private _nextTokenId;
     mapping(uint256 => bool) private _uniqueIdUsed;
 
